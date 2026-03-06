6fa1fb0d64c383e19913fc015c21fd2782d2cef3
tsuna23222 <richardcasinilloph@gmail.com>
2026-03-06 11:21:41 +0800

    index js

--- index.js
+++ index.js
@@ -17,9 +17,15 @@ app.get('/about', (req, res) => {
 });
 
 app.use(express.json());
+
 app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 });
 
+
+app.use((req, res) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
