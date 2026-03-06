43328cfc2b6c5f10b8c34bed404828c35b44ea63
tsuna23222 <richardcasinilloph@gmail.com>
2026-03-06 11:06:04 +0800

    commit all

---
+++ index.js
@@ -0,0 +1,25 @@
+const express  = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`); 
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.use(express.json());
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+
